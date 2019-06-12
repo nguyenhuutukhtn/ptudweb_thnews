c383e744fcdb2fcd03fbbd684ad95e5ccee840db
nguyenhuutukhtn <nguyenhuutu.khtn@gmail.com>
2019-06-12 18:26:55 +0700

    add lastest news middleware

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+var express=require('express');
+var exphbs=require('express-handlebars');
+
+var app=express();
+
+app.use(express.static('public'));
+
+app.engine('hbs',exphbs({
+    defaultLayout:'main_public.hbs',
+    layoutsDir:'views/layouts'
+}));
+
+app.set('view engine','hbs');
+
+
+app.use(require('./middlewares/lastestNews.mdw'));
+
+app.use('/',require('./routes/homepage.route'));
+
+
+app.listen(3000,()=>{
+    console.log('server is running at http://localhost:3000');
+})
--- models/article.model.js
+++ models/article.model.js
@@ -3,5 +3,11 @@ var db=require('../utils/db');
 module.exports={
     all:()=>{
         return db.load('select * from article');
+    },
+
+    FourLastestNew:()=>{
+        return db.load('SELECT * FROM article ORDER BY PublishDay desc LIMIT 4');
     }
+
+    
 };
