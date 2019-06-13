7ecc991002c11beca9770cc8adae806a4275e27a
Tin Huynh <trungtin2qn1@gmail.com>
2019-06-13 20:32:34 +0700

    Add authentication raw feature

--- routes/homepage.route.js
+++ routes/homepage.route.js
@@ -1,16 +1,53 @@
-var express=require('express');
-var articleModel=require('../models/article.model');
+var express = require('express');
+var articleModel = require('../models/article.model');
+var tokenHandler = require('../middlewares/tokenHandler');
+var cookieParser = require('cookie-parser')
 
-var router=express.Router();
+var router = express.Router();
+var bodyParser = require('body-parser');
 
-router.get('/',(req,res)=>{
-    var p=articleModel.all();
-    p.then(rows=>{
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(cookieParser())
+// parse application/json
+router.use(bodyParser.json());
+
+router.get('/', (req, res) => {
+    var p = articleModel.all();
+    p.then(rows => {
         res.render('home')
         //console.log(rows);
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err);
     });
 })
 
-module.exports=router;
+router.get('/verify', async (req, res) => {
+    var token = req.cookies['Authorization'];
+    await tokenHandler.verify(req, res, token);
+    if (res.err != null){
+        res.status(401).send({
+            message: "Unauthorized"
+        })
+    } else {
+        res.status(200).send({
+            message: "Ok"
+        })
+    }
+})
+
+router.post('/register', async (req, res) => {
+    var err = await tokenHandler.issue(req, res);
+    if (err == false) {
+        res.status(401).send({
+            message: "Unauthorized"
+        })
+    } else {
+        console.log(res.token);
+        res.cookie('Authorization', res.token, { maxAge: 900000, httpOnly: true });
+        res.status(200).send({
+            token: res.token
+        })
+    }
+})
+
+module.exports = router;
--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ var createConnection = () => {
     return mysql.createConnection({
         host: 'localhost',
         port: '3306',
-        user: 'thnews_admin',
+        user: 'root',
         password: '123456',
         database: 'th_news'
     });
