8f9c3e2fab5e9511818c09885d39a580ac6c52bf
Tin Huynh <trungtin2qn1@gmail.com>
2019-06-17 17:25:11 +0700

    Release authentication features

--- routes/homepage.route.js
+++ routes/homepage.route.js
@@ -9,7 +9,7 @@ var session = require('express-session');
 var flash = require('connect-flash');
 var subscriber = require('../controllers/subscriber.controller');
 var FacebookStrategy = require('passport-facebook').Strategy;
-//var GoogleStategy = require('passport-google-auth').OAuth2Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 var guestRole = require('../middlewares/guestRole.mdw');
 var editorRole = require('../middlewares/editorRole.mdw');
 var bcrypt = require('bcryptjs');
@@ -47,13 +47,13 @@ router.use(require('../middlewares/feadturedWritter.mdw'))
 //Trash. Just!... I don't know maybe it's just another stressful day
 
 // Passport session setup.
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user);
-  });
-  
-  passport.deserializeUser(function(obj, done) {
+});
+
+passport.deserializeUser(function (obj, done) {
     done(null, obj);
-  });
+});
 
 passport.use(new FacebookStrategy({
     // điền thông tin để xác thực với Facebook.
@@ -78,14 +78,16 @@ passport.use(new FacebookStrategy({
                         var temp;
                         authentication.issueTokenWithUser(user, temp)
                             .then(token => {
+                                console.log("Have itttttttttttttttt");
+                                console.log("token: ", token);
                                 success = myCache.set("token", token, 10000);
                                 return done(null, user);
                             })
                             .catch(err => {
+                                console.log("Have itttttttttttttttt");
                                 console.log(err);
                                 return done(err);
                             })
-                        console.log("Have itttttttttttttttt");
                     } else {
                         var hashedPassword = bcrypt.hashSync('123', 8);
                         console.log(profile);
@@ -98,6 +100,73 @@ passport.use(new FacebookStrategy({
                         }
                         // // lưu các thông tin cho user
                         // lưu vào db
+                        var temp;
+                        authentication.register(newUser, 'guests', temp)
+                            .then(token => {
+                                console.log("Start cache");
+                                console.log("token: ", token);
+                                success = myCache.set("token", token, 10000);
+                                console.log("End cache");
+                                return done(null, newUser);
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                return done(err);
+                            })
+                    }
+                })
+                .catch(err => {
+                    console.log("errr");
+                    console.log(err);
+                    return done(err);
+                })
+        });
+    })
+);
+
+passport.use(new GoogleStrategy({
+    // điền thông tin để xác thực với Facebook.
+    // những thông tin này đã được điền ở file auth.js
+    clientID: configAuth.google.app_id,
+    clientSecret: configAuth.google.app_secret,
+    callbackURL: configAuth.google.call_back,
+},
+    // Facebook sẽ gửi lại chuối token và thông tin profile của user
+    function (token, refreshToken, profile, done) {
+        // asynchronous
+        console.log(1);
+        process.nextTick(function () {
+            // tìm trong db xem có user nào đã sử dụng facebook id này chưa
+            console.log(2);
+            console.log("profile: ", profile);
+            userModel.getByGoogleID(profile.id)
+                .then(user => {
+                    console.log(user);
+                    if (user.length !== 0) {
+                        var temp;
+                        authentication.issueTokenWithUser(user, temp)
+                            .then(token => {
+                                success = myCache.set("token", token, 10000);
+                                return done(null, user);
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                return done(err);
+                            })
+                        console.log("Have itttttttttttttttt");
+                    } else {
+                        var hashedPassword = bcrypt.hashSync('123', 8);
+                        console.log(profile);
+                        console.log('avatar: ', profile.photos[0].value)
+                        var newUser = {
+                            facebook_id: profile.id,
+                            email: profile.emails[0].value,
+                            password: hashedPassword,
+                            first_name: profile.name.givenName,
+                            last_name: profile.name.familyName,
+                            avatar: profile.photos[0].value,
+                        }
+                        console.log("Save user to data");
                         var temp;
                         authentication.register(newUser, 'guests', temp)
                             .then(token => {
@@ -121,78 +190,21 @@ passport.use(new FacebookStrategy({
     })
 );
 
-// passport.use(new GoogleStategy({
-//     // điền thông tin để xác thực với Facebook.
-//     // những thông tin này đã được điền ở file auth.js
-//     clientID: configAuth.google.app_id,
-//     clientSecret: configAuth.google.app_secret,
-//     callbackURL: configAuth.google.call_back,
-// },
-//     // Facebook sẽ gửi lại chuối token và thông tin profile của user
-//     function (token, refreshToken, profile, done) {
-//         // asynchronous
-//         console.log(1);
-//         process.nextTick(function () {
-//             // tìm trong db xem có user nào đã sử dụng facebook id này chưa
-//             console.log(2);
-//             console.log("profile: ", profile);
-//             userModel.getByGoogleID(profile.id)
-//                 .then(user => {
-//                     console.log(user);
-//                     if (user.length !== 0) {
-//                         var temp;
-//                         authentication.issueTokenWithUser(user, temp)
-//                             .then(token => {
-//                                 success = myCache.set("token", token, 10000);
-//                                 return done(null, user);
-//                             })
-//                             .catch(err => {
-//                                 console.log(err);
-//                                 return done(err);
-//                             })
-//                         console.log("Have itttttttttttttttt");
-//                     } else {
-//                         var hashedPassword = bcrypt.hashSync('123', 8);
-//                         console.log(profile);
-//                         var newUser = {
-//                             facebook_id: profile.id,
-//                             email: profile.emails[0].value,
-//                             password: hashedPassword,
-//                             first_name: profile.name.givenName,
-//                             last_name: profile.name.familyName
-//                         }
-//                         // // lưu các thông tin cho user
-//                         // lưu vào db
-//                         var temp;
-//                         authentication.register(newUser, 'guests', temp)
-//                             .then(token => {
-//                                 console.log("Start cache");
-//                                 success = myCache.set("token", token, 10000);
-//                                 console.log("End cache");
-//                                 return done(null, newUser);
-//                             })
-//                             .catch(err => {
-//                                 console.log(err);
-//                                 return done(err);
-//                             })
-//                     }
-//                 })
-//                 .catch(err => {
-//                     console.log("errr");
-//                     console.log(err);
-//                     return done(err);
-//                 })
-//         });
-//     })
-// );
-
-
 //-----------------------------------------------
 
 router.use('/guest', guestRole);
 //router.use('/editor', editorRole);
 
 router.get('/', (req, res) => {
+    value = myCache.get("token");
+    console.log(value);
+    if (value == undefined) {
+        // handle miss!
+        console.log("Empty cache");
+    } else {
+        console.log("token cookies : ", value);
+        res.cookie('Authorization', value, { maxAge: 900000, httpOnly: true });
+    }
     var p = articleModel.all();
     p.then(rows => {
         res.render('home')
@@ -313,25 +325,38 @@ router.post('/logout', (req, res) => {
     authentication.verify(token, res)
         .then(temp => {
             res.clearCookie('Authorization');
-            res.redirect('/');
+            value = myCache.del("token");
             res.render('home');
+            res.redirect('/');
         })
         .catch(err => {
             res.render('401');
         })
 })
 
-router.get('/authentication/facebook/callback',
-    passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }),
+router.get('/authentication/google/callback',
+    passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }),
     function (req, res) {
         value = myCache.get("token");
         if (value == undefined) {
             // handle miss!
             console.log("Empty cache");
         }
+        console.log("token: ", value);
         res.cookie('Authorization', value, { maxAge: 900000, httpOnly: true });
         res.redirect('/');
     });
+
+router.get('/authentication/facebook/callback',
+    passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }),
+    function (req, res) {
+    });
+
+router.get('/authentication/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
 
 router.get('/authentication/facebook',
     passport.authenticate('facebook', {
