d4a214098f8699555a2b8691fc4bea3ae3e929c5
leminhtrihcmus <trilqdola@gmail.com>
2019-06-18 14:03:13 +0700

    giao dien

--- routes/search.route.js
+++ routes/search.route.js
@@ -13,4 +13,8 @@ router.use(require('../middlewares/LastestPost.mdw'))
 router.use(require('../middlewares/FourLastestNews.mdw'))
 router.use(require('../middlewares/TopThreeHot.mdw'))
 
+router.get('/', (req, res) => {
+    res.render('search')
+})
+
 module.exports = router;
