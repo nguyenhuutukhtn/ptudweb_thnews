7e9c6d00a22bb0a03c5668c9db472995f9d39c40
Tin Huynh <trungtin2qn1@gmail.com>
2019-06-18 21:05:43 +0700

    Update home page

--- routes/article.route.js
+++ routes/article.route.js
@@ -23,49 +23,68 @@ router.get('/:id', (req, res) => {
     var id = req.params.id;
     var p = articleModel.articleDetail(id);
 
+    console.log(0);
     p.then(articleDetail => {
-        var q=articleModel.RelateNews(id);
-        q.then(RelateNews=>{
-            var r=tagModel.AllTags(id);
-            r.then(AllTags=>{
-                var t=usersModel.Writer(id);
-                t.then(Writer=>{
-                    var x=commentModel.AllComments(id);
-                    x.then(AllComments=>{
-                        res.render('article_details',{
-                            relateNews:RelateNews,
-                            articleDetail:articleDetail,
-                            AllTags:AllTags,
-                            Writer:Writer,
-                            AllComments:AllComments
+        var q = articleModel.RelateNews(id);
+        console.log(1);
+        q.then(RelateNews => {
+            var r = tagModel.AllTags(id);
+            console.log(2);
+            r.then(AllTags => {
+                console.log(3);
+                var t = usersModel.Writer(id);
+                t.then(Writer => {
+                    console.log(4);
+                    var x = commentModel.AllComments(id);
+                    x.then(AllComments => {
+                        console.log(5);
+                        if (articleDetail[0].isPremium === 1) {
+                            subscriberController.verify(res.id)
+                                .then(result => {
+                                    console.log(6);
+                                    if (result === false) {
+                                        res.render('/401');
+                                        return
+                                    }
+                                })
+                                .catch(err => {
+                                    console.log(7);
+                                    console.log(err);
+                                    res.render('/500');
+                                    return
+                                })
+                        }
+                        res.render('article_details', {
+                            relateNews: RelateNews,
+                            articleDetail: articleDetail,
+                            AllTags: AllTags,
+                            Writer: Writer,
+                            AllComments: AllComments
                         });
                     })
                 })
-            })    
-        })
-            
-        if (articleDetail[0].isPremium === 1) {
-            subscriberController.verify(res.id)
-                .then(result => {
-                    if (result === false) {
-                        res.render('/401');
-                    }
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.render('/500');
-                })
-        }
-        var q = articleModel.RelateNews(id);
-        q.then(RelateNews => {
-            res.render('article_details', {
-                relateNews: RelateNews,
-                articleDetail: articleDetail
-            });
+            })
         })
 
+        // if (articleDetail[0].isPremium === 1) {
+        //     subscriberController.verify(res.id)
+        //         .then(result => {
+        //             console.log(6);
+        //             if (result === false) {
+        //                 res.render('/401');
+        //                 return
+        //             }
+        //         })
+        //         .catch(err => {
+        //             console.log(7);
+        //             console.log(err);
+        //             res.render('/500');
+        //             return
+        //         })
+        // }
         //console.log(rows);
     }).catch(err => {
+        console.log(10);
         console.log(err);
     });
 })
--- routes/forgot.route.js
+++ routes/forgot.route.js
@@ -1,40 +1,104 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
-var authentication = require('../controllers/authentication.controller');
-
+var forgotController = require('../controllers/forgot.controller');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var flash = require('connect-flash');
+var session = require('express-session');
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(cookieParser())
 router.use(bodyParser.json());
 
+router.use(session({ secret: "_____secret_key______" }));
+
 router.use(require('../middlewares/LastestPost.mdw'))
 router.use(require('../middlewares/FourLastestNews.mdw'))
 router.use(require('../middlewares/TopThreeHot.mdw'))
+
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
+
+router.use(flash()); // use connect-flash for flash messages stored in session
 
 router.get('/', (req, res) => {
     res.render('forgot');
 })
 
-router.post('/admin', (req, res) => {
-    var email = req.body.email
-    var password = req.body.password
-    console.log("email:", email);
-    console.log("password: ", password);
-    authentication.login(email, password)
+router.post('/', (req, res) => {
+
+    var email = req.body.email;
+
+    console.log("email: ", email);
+
+    var smtpTransport = nodemailer.createTransport({
+        service: 'sendgrid',
+        auth: {
+            user: 'trungtin2qn1',
+            pass: 'tin123456'
+        }
+        //service: 'gmail',
+        //host: "smtp.gmail.com",
+        //secure: true,
+        // auth: {
+        //     type: 'login', // default
+        //     user: 'trunghieu0697@gmail.com',
+        //     pass: 'qwerty12345qwerty'
+        // }
+    });
+
+    console.log(0);
+
+    var mailOptions = {
+        to: email,
+        from: 'trungtin2qn1@gmail.com',
+        subject: 'Node.js Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+            'http://localhost:3000/forgot/reset/' + email + '\n\n' +
+            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+    };
+
+    console.log(1);
+
+    smtpTransport.sendMail(mailOptions, function (err) {
+        console.log(err);
+        req.flash('info', 'An e-mail has been sent to ' + email + ' with further instructions.');
+        console.log(2);
+        res.redirect('/');
+    });
+});
+
+router.get('/reset', (req, res) => {
+    res.render('reset');
+})
+
+router.post('/reset', (req, res) => {
+    console.log(req.headers);
+    var newPassword = req.body.new_password;
+    var confirmPassword = req.body.confirm_password;
+    console.log("new password: ", newPassword);
+    console.log("confirm password: ", confirmPassword);
+    var email = myCache.get('reset-email');
+    console.log("email: ",email);
+    forgotController.reset(email, newPassword, confirmPassword)
         .then(token => {
-            res.cookie('Authorization', token, { maxAge: 900000, httpOnly: true });
+            res.cookie('Authorization', token, { maxAge: 900000 * 10, httpOnly: true });
             console.log("Success: ", token);
             res.redirect('/');
         })
         .catch(err => {
-            res.redirect('/401');
+            console.log(err);
+            res.redirect('/500');
         })
 })
 
-router.post('/', (req, res) =>{
-
-});
+router.get('/reset/:email', (req, res) => {
+    var email = req.params.email;
+    myCache.set('reset-email', email);
+    console.log("email: ", email);
+    res.redirect('/forgot/reset');
+})
 
 module.exports = router;
